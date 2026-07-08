b9996c41febba7187c791018746dec9576565e7d
BorpBorp (Filoselle) <47865495+BorpBorp@users.noreply.github.com>
2026-07-07 23:15:35 -0400

    Converted to MetaTable two new functions

--- src/main/java/LuaCraft/LuaStom/sandbox/inventory/AbstractInventoryLib.java
+++ src/main/java/LuaCraft/LuaStom/sandbox/inventory/AbstractInventoryLib.java
@@ -2,27 +2,87 @@ package LuaCraft.LuaStom.sandbox.inventory;
 
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.ThreeArgFunction;
 import org.luaj.vm2.lib.TwoArgFunction;
 
+import LuaCraft.LuaStom.LuaErrorAssert;
 import net.minestom.server.inventory.AbstractInventory;
 import net.minestom.server.item.ItemStack;
 
 public class AbstractInventoryLib extends LuaTable {
     private AbstractInventory inventory;
 
-    public AbstractInventoryLib(AbstractInventory inventory) {
-        this.inventory = inventory;
+    private static final LuaTable INVENTORY_METATABLE = new LuaTable();
 
-        rawset("AddItemStack", new TwoArgFunction() {
+    static {
+        INVENTORY_METATABLE.set("__index", INVENTORY_METATABLE);
+
+        INVENTORY_METATABLE.set("AddItem", new TwoArgFunction() {
             @Override
             public LuaValue call(LuaValue self, LuaValue item) {
-                ItemStack itemstack = ((ItemStackLib) item).getItemStack();
+                if (self instanceof AbstractInventoryLib inv) {
+                    ItemStack itemstack = ((ItemStackLib) item).getItemStack();
+                    AbstractInventory inventory = inv.getInventory();
 
-                inventory.addItemStack(itemstack);
+                    inventory.addItemStack(itemstack);
 
-                return AbstractInventoryLib.this;
+                    return self;
+                } else {
+                    return LuaValue.NIL;
+                }
             }
         });
+
+        INVENTORY_METATABLE.set("RemoveItemAmount", new ThreeArgFunction() {
+            @Override
+            public LuaValue call(LuaValue self, LuaValue item, LuaValue amount) {
+                if (!(self instanceof AbstractInventoryLib inv)) return LuaValue.NIL;
+                if (!(item instanceof ItemStackLib itemstack)) return LuaValue.NIL;
+
+                ItemStack itemStack = itemstack.getItemStack();
+                AbstractInventory inventory = inv.getInventory();
+                int amountToRemove = LuaErrorAssert.checkInt(amount, "Inventory:RemoveItemAmount", 2);
+
+                for (int i = 0; i < inventory.getSize(); i++) {
+                    ItemStack otherItem = inventory.getItemStack(i);
+
+                    if (otherItem.material() == itemStack.material()) {
+                        int currentAmount = otherItem.amount();
+
+                        if (currentAmount <= amountToRemove) {
+                            inventory.setItemStack(i, ItemStack.AIR);
+                            amountToRemove -= currentAmount;
+                        } else {
+                            inventory.setItemStack(i, otherItem.withAmount(currentAmount - amountToRemove));
+                            amountToRemove = 0;
+                            break;
+                        }
+                    }
+                }
+
+                return self;
+            }
+        });
+
+        INVENTORY_METATABLE.set("SetSlot", new ThreeArgFunction() {
+            @Override
+            public LuaValue call(LuaValue self, LuaValue slot, LuaValue item) {
+                if (!(self instanceof AbstractInventoryLib inv)) return LuaValue.NIL;
+                if (!(item instanceof ItemStackLib itemstack)) return LuaValue.NIL;
+
+                ItemStack itemStack = itemstack.getItemStack();
+                AbstractInventory inventory = inv.getInventory();
+
+                inventory.setItemStack(LuaErrorAssert.checkInt(slot, "Inventory:SetSlot", 1), itemStack);
+
+                return self;
+            }
+        });
+    }
+
+    public AbstractInventoryLib(AbstractInventory inventory) {
+        this.inventory = inventory;
+        this.setmetatable(INVENTORY_METATABLE);
     }
 
     public AbstractInventory getInventory() {
